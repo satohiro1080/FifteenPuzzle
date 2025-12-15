a6e7b7547be5f263fd52cd7e58618729cbffb151
moneyyyyyMan <k235002@kccollege.ac.jp>
2025-12-15 11:29:29 +0900

    AnswerBoxを3色で表示

--- project/Source/Answer.cpp
+++ project/Source/Answer.cpp
@@ -1,11 +1,47 @@
 #include "Answer.h"
+#include "Screen.h"
+
+namespace AnswerConfig
+{
+	VECTOR2 boxSize = VECTOR2(150.0f, 100.0f);
+}
 
 Answer::Answer()
 {
-
+	float ofset = 64.0f;
+	// äÓèÄì_Ç…Ç»ÇÈàÍî‘ç∂ÇÃBox
+	answerBox[0].position = VECTOR2(ofset, Screen::HEIGHT - Screen::HEIGHT / 4.0f);
+	answerBox[1].position = VECTOR2(answerBox[0].position.x + AnswerConfig::boxSize.x + ofset, Screen::HEIGHT - Screen::HEIGHT / 4.0f);
+	answerBox[2].position = VECTOR2(answerBox[1].position.x + AnswerConfig::boxSize.x + ofset, Screen::HEIGHT - Screen::HEIGHT / 4.0f);
 }
 
 Answer::~Answer()
 {
 
+}
+
+void Answer::Update()
+{
+	for (int i = 0; i < MAX_BOX_NUM; i++)
+	{
+		if (answerBox[i].checkHit)
+		{
+			answerBox[i].position.y = answerBox[i].position.y + 50.0f;
+		}
+	}
+}
+
+void Answer::Draw()
+{
+	// âºê‘î†
+	DrawBoxAA(answerBox[0].position.x, answerBox[0].position.y,
+		answerBox[0].position.x + AnswerConfig::boxSize.x, answerBox[0].position.y + AnswerConfig::boxSize.y, GetColor(255, 0, 0), TRUE);
+
+	// âºóŒî†
+	DrawBoxAA(answerBox[1].position.x, answerBox[1].position.y,
+		answerBox[1].position.x + AnswerConfig::boxSize.x, answerBox[1].position.y + AnswerConfig::boxSize.y, GetColor(0, 255, 0), TRUE);
+
+	// âºê¬î†
+	DrawBoxAA(answerBox[2].position.x, answerBox[2].position.y,
+		answerBox[2].position.x + AnswerConfig::boxSize.x, answerBox[2].position.y + AnswerConfig::boxSize.y, GetColor(0, 0, 255), TRUE);
 }
--- project/Source/Answer.h
+++ project/Source/Answer.h
@@ -6,11 +6,20 @@ class Answer : public GameObject
 public:
 	Answer();
 	~Answer();
-
+	void Update() override;
+	void Draw() override;
 
 
 private:
-
+	static const int MAX_BOX_NUM = 3;
+	struct ANSWER_BOX
+	{
+		VECTOR2 position;	// 座標
+		float exRate;		// 拡大率
+		bool checkHit;		// マウスポインターが触れているか
+		bool select;		// 選択状態を保存
+	};
+	ANSWER_BOX answerBox[MAX_BOX_NUM];
 
 
 
