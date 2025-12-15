fdc98fc8133427a2901dc69de457ee01b5f21827
satohiro <k235011@kccollege.ac.jp>
2025-12-15 11:17:27 +0900

    カーソル位置表示とクリック表示

--- project/Source/MouseInputManager.cpp
+++ project/Source/MouseInputManager.cpp
@@ -1,11 +1,57 @@
 #include "MouseInputManager.h"
+#include <DxLib.h>
+
+namespace
+{
+	static const int CURSOR_RADIUS = 16;
+	static const int CURSOR_COLOR_NORMAL = 0xFFAAAA;
+	static const int CURSOR_COLOR_CLICK = 0xFF0000;
+	static const int CURSOR_ALPHA = 100;
+}
 
 MouseInputManager::MouseInputManager()
 {
-
+	m_cursorPosition = VECTOR2(0, 0);
+	m_isClick = false;
 }
 
 MouseInputManager::~MouseInputManager()
 {
 
+}
+
+void MouseInputManager::Update()
+{
+	int x, y;
+	GetMousePoint(&x, &y);
+
+	m_cursorPosition = VECTOR2((float)x, (float)y);
+
+	if ((GetMouseInput() & MOUSE_INPUT_LEFT) != 0)
+	{
+		m_isClick = true;
+	}
+	else
+	{
+		m_isClick = false;
+	}
+}
+
+void MouseInputManager::Draw()
+{
+	SetDrawBlendMode(DX_BLENDMODE_ALPHA, CURSOR_ALPHA);
+	{
+		int x = (int)m_cursorPosition.x;
+		int y = (int)m_cursorPosition.y;
+		int color = m_isClick ?
+			CURSOR_COLOR_CLICK :
+			CURSOR_COLOR_NORMAL;
+		DrawCircle(x, y, CURSOR_RADIUS, color);
+	}
+	SetDrawBlendMode(DX_BLENDMODE_NOBLEND, 0);
+}
+
+bool MouseInputManager::IsClickArea(const VECTOR2& areaPos, const VECTOR2& areaSize)
+{
+	return false;
 }
--- project/Source/MouseInputManager.h
+++ project/Source/MouseInputManager.h
@@ -6,12 +6,20 @@ class MouseInputManager : public GameObject
 public:
 	MouseInputManager();
 	~MouseInputManager();
+	void Update() override;
+	void Draw() override;
 
-
+	/// <summary>
+	/// ※工事中！　　ボタンUI用 矩形内をクリックしたか取得
+	/// </summary>
+	/// <param name="areaPos">矩形の始点</param>
+	/// <param name="areaSize">矩形の大きさ</param>
+	/// <returns>クリックしたらtrue</returns>
+	bool IsClickArea(const VECTOR2& areaPos, const VECTOR2& areaSize);
 
 private:
-
-
+	VECTOR2 m_cursorPosition;
+	bool m_isClick;
 
 
 };
