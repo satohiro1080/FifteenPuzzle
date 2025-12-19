31168691c50b0d6157d3cf2f4690d57e24ca3fb8
satohiro <k235011@kccollege.ac.jp>
2025-12-19 19:10:28 +0900

    QuizManager 問題を取得するGetNextQuiz関数など追加（中身まだ）

--- project/Source/QuizManager.cpp
+++ project/Source/QuizManager.cpp
@@ -7,3 +7,17 @@ QuizManager::QuizManager()
 QuizManager::~QuizManager()
 {
 }
+
+void QuizManager::ShuffleQuizQue()
+{
+}
+
+Quiz::Data QuizManager::GetNextQuiz()
+{
+	return Quiz::Data();
+}
+
+Quiz::Data QuizManager::GetQuizWithID(int id) const
+{
+	return Quiz::Data();
+}
--- project/Source/QuizManager.h
+++ project/Source/QuizManager.h
@@ -1,6 +1,10 @@
 #pragma once
+#include "QuizDefine.h"
+#include <vector>
+#include <list>
 
 // 全問題のデータを管理する
+// 基本は GetNextQuiz() から問題を取得してね
 // もしかしたら生成場所コモンシーンにするかも
 class QuizManager
 {
@@ -8,6 +12,17 @@ public:
 	QuizManager();
 	~QuizManager();
 
-private:
+	// クイズの順番をシャッフルする
+	void ShuffleQuizQue();
 
+	// 次のクイズを取得
+	// 一度出たクイズは、次にシャッフルされるまで出ない
+	// 全てのクイズが出た状態でこの関数を呼ぶと、シャッフルされる
+	Quiz::Data GetNextQuiz();
+
+	// クイズの順番を無視して、指定IDのクイズを取得
+	Quiz::Data GetQuizWithID(int id) const;
+private:
+	std::vector<Quiz::Data> m_quizDatas; // クイズのデータ
+	std::list<int> m_quizQue;	// クイズの順番リスト
 };
