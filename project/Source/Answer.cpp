718976f3f4d787b393fcc28b92ec722e5ee7e596
moneyyyyyMan <k235002@kccollege.ac.jp>
2025-12-22 10:29:28 +0900

    シャッター追加

--- project/Source/Answer.cpp
+++ project/Source/Answer.cpp
@@ -2,11 +2,15 @@
 #include "Screen.h"
 
 #include "MouseInputManager.h"
+#include "Board.h"
 
 namespace AnswerConfig
 {
 	VECTOR2 boxSize = VECTOR2(150.0f, 100.0f);
 	float EXRATE = 1.25f;
+	int RED_NUM = 0;
+	int GREEN_NUM = 3;
+	int BLUE_NUM = 12;
 }
 
 Answer::Answer()
@@ -23,6 +27,7 @@ Answer::Answer()
 	answerBox[BLUE].backHandler = LoadGraph("data/texture/AnswerBoxBlue_Back.png");
 	answerBox[BLUE].exRate = AnswerConfig::EXRATE;
 
+	shutter = LoadGraph("data/texture/cloased.png");
 
 	float ofset = 64.0f;
 	// 基準点になる一番左のBox
@@ -30,12 +35,23 @@ Answer::Answer()
 	answerBox[GREEN].position = VECTOR2(answerBox[RED].position.x + AnswerConfig::boxSize.x + ofset, Screen::HEIGHT - Screen::HEIGHT / 4.0f + AnswerConfig::boxSize.y / 2.0f);
 	answerBox[BLUE].position = VECTOR2(answerBox[GREEN].position.x + AnswerConfig::boxSize.x + ofset, Screen::HEIGHT - Screen::HEIGHT / 4.0f + AnswerConfig::boxSize.y / 2.0f);
 
+	answerBox[RED].panelNum = AnswerConfig::RED_NUM;
+	answerBox[GREEN].panelNum = AnswerConfig::GREEN_NUM;
+	answerBox[BLUE].panelNum = AnswerConfig::BLUE_NUM;
+
+	answerBox[RED].text = "あああああ";
+	answerBox[GREEN].text = "あああああ";
+	answerBox[BLUE].text = "あああああ";
+
 	for (int i = 0; i < MAX_BOX_NUM; i++)
 	{
 		answerBox[i].animTime = 0.0f;
+		answerBox[i].select = false;
+		answerBox[i].shutterExRate = 1.75f;
 	}
 
 	mouseInput = nullptr;
+	board = nullptr;
 }
 
 Answer::~Answer()
@@ -53,15 +69,49 @@ void Answer::Update()
 	{
 		mouseInput = FindGameObject<MouseInputManager>();
 	}
+	
+	if (board == nullptr)
+	{
+		board = FindGameObject<Board>();
+	}
 
 	for (int i = 0; i < MAX_BOX_NUM; i++)
 	{
-		if (mouseInput->IsClickArea(answerBox[i].position - AnswerConfig:: boxSize / 2.0f,AnswerConfig::boxSize))
+		// 対象のパネルがハマったらセレクト可能にする
+		if (board != nullptr)
 		{
-			//answerBox[i].position.y = answerBox[i].position.y - 50.0f;
+			if (board->IsCorrectPos(answerBox[i].panelNum))
+			{
+				answerBox[i].select = true;
+			}
+			else
+			{
+				answerBox[i].select = false;
+			}
+		}
 
-			// ここで選択した配列番号を送るよ
-			// 例:GameManager->GetAnswer(i); [配列番号 = 選択した番号]　になっているからこれだけでできるかも
+		if (answerBox[i].select)
+		{
+			if (answerBox[i].shutterExRate >= 0.0f)
+			{
+				answerBox[i].shutterExRate -= 0.1f;
+			}
+
+			if (mouseInput->IsClickArea(answerBox[i].position - AnswerConfig::boxSize / 2.0f, AnswerConfig::boxSize))
+			{
+				//answerBox[i].position.y = answerBox[i].position.y - 50.0f;
+
+				// ここで選択した配列番号を送るよ
+				// 例:GameManager->GetAnswer(i); [配列番号 = 選択した番号]　になっているからこれだけでできるかも
+				break;
+			}
+		}
+		else
+		{
+			if (answerBox[i].shutterExRate <= 1.75f)
+			{
+				answerBox[i].shutterExRate += 0.5f;
+			}
 		}
 
 		if (mouseInput->IsMouseOverArea(answerBox[i].position - AnswerConfig::boxSize / 2.0f, AnswerConfig::boxSize))
@@ -90,10 +140,16 @@ void Answer::Draw()
 		AnswerConfig::boxSize.x / 2.0f, AnswerConfig::boxSize.y / 2.0f, answerBox[BLUE].exRate, answerBox[BLUE].exRate, 0.0f, answerBox[BLUE].backHandler, TRUE);
 
 	// 回答の画像表示
+	DrawFormatString(answerBox[RED].position.x - 64.0f, answerBox[RED].position.y, GetColor(255, 255, 255), answerBox[RED].text.c_str());
+	DrawFormatString(answerBox[GREEN].position.x - 64.0f, answerBox[GREEN].position.y, GetColor(255, 255, 255), answerBox[GREEN].text.c_str());
+	DrawFormatString(answerBox[BLUE].position.x - 64.0f, answerBox[BLUE].position.y, GetColor(255, 255, 255), answerBox[BLUE].text.c_str());
 
 
-
-
+	for (int i = 0; i < MAX_BOX_NUM; i++)
+	{
+		DrawRectRotaGraph3F(answerBox[i].position.x, answerBox[i].position.y - 55.0f, 0, 0, AnswerConfig::boxSize.x, AnswerConfig::boxSize.y,
+			AnswerConfig::boxSize.x / 2.0f, AnswerConfig::boxSize.y / 2.0f, answerBox[i].exRate, answerBox[i].shutterExRate, 0.0f, shutter, TRUE);
+	}
 
 	// 回答選択肢の枠組み
 	DrawRectRotaGraph3F(answerBox[RED].position.x, answerBox[RED].position.y, 0, 0, AnswerConfig::boxSize.x, AnswerConfig::boxSize.y,
--- project/Source/Answer.h
+++ project/Source/Answer.h
@@ -3,6 +3,7 @@
 
 class MouseInputManager;
 class GameManager;
+class Board;
 
 class Answer : public GameObject
 {
@@ -11,6 +12,8 @@ public:
 	~Answer();
 	void Update() override;
 	void Draw() override;
+
+	void GetBoard(Board* _board) { board = _board; };
 
 	enum COLOR_TYPE
 	{
@@ -28,12 +31,18 @@ private:
 		int backHandler;
 		VECTOR2 position;	// 座標
 		float exRate;		// 拡大率
+		float shutterExRate;
 		float animTime;		// 拡縮アニメーション用
-		bool checkHit;		// マウスポインターが触れているか
-		bool select;		// 選択状態を保存
+		int panelNum;		// 回答表示するために合致する必要のあるパネルの番号
+		bool select;	// 選択可能かみる
+		std::string text;
+
 	};
 	ANSWER_BOX answerBox[MAX_BOX_NUM];
 
+	int shutter;
+
 	MouseInputManager* mouseInput;
+	Board* board;
 
 };
