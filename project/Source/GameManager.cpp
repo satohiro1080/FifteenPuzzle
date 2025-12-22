9ea7c50c0b7e8e74fa7cbbbcb8ced78f76995726
Felix <k235014@kccollege.ac.jp>
2025-12-22 10:31:00 +0900

    GameManagerで問題の取得、ユーザーの解答取得を追加

--- project/Source/GameManager.cpp
+++ project/Source/GameManager.cpp
@@ -1,10 +1,12 @@
 #include "GameManager.h"
+#include "QuizManager.h"
+
 
 GameManager::GameManager()
 {
 	displayTime = 300;
 	resultTime = 120;
-
+	g_state = GS_START;
 }
 
 GameManager::~GameManager()
@@ -13,47 +15,44 @@ GameManager::~GameManager()
 
 void GameManager::Update()
 {
-	;
+	m_quizManager = FindGameObject<QuizManager>();
 	switch (g_state)
 	{
-	case GS_START://ゲームの開始＆次の問題へ移動
-		switch (q_num)
+	case GS_START://ゲームの開始＆次のクイズを取得(QuizManagerで問題番号を取得)		
+		quiz = m_quizManager->GetNextQuiz();
+		switch (quiz.ID)
 		{
-		case Q_NULL:
-			q_num = Q_1;
+		case 0:
 			correctAnswerNo = ANSWER_NUMBER_1;
 			g_state = GS_ANSWERING;
 			break;
-		case Q_1:
-			q_num = Q_2;
+		case 1:
 			correctAnswerNo = ANSWER_NUMBER_2;
 			g_state = GS_ANSWERING;
 			break;
-		case Q_2:
-			q_num = Q_3;
+		case 2:
 			correctAnswerNo = ANSWER_NUMBER_3;
 			g_state = GS_ANSWERING;
 			break;
-		case Q_3:
-			break;
 		}
+
 		break;
 	case GS_ANSWERING://回答フェイズ
 		if (pushed == false)
 		{
 			if (CheckHitKey(KEY_INPUT_Z))//回答1(仮)
 			{
-				answerNo = 1;
+				answerNo = 0;
 				g_state = GS_ANSWER;
 			}
 			if (CheckHitKey(KEY_INPUT_X))//回答2(仮)
 			{
-				answerNo = 2;
+				answerNo = 1;
 				g_state = GS_ANSWER;
 			}
 			if (CheckHitKey(KEY_INPUT_C))//回答3(仮)
 			{
-				answerNo = 3;
+				answerNo = 2;
 				g_state = GS_ANSWER;
 			}
 		}
@@ -81,6 +80,7 @@ void GameManager::Update()
 			else
 			{
 				g_state = GS_START;
+				//m_quizManager->GetNextQuiz();
 				//answer = false;
 				displayTime = 300;
 				resultTime = 120;
@@ -97,6 +97,7 @@ void GameManager::Update()
 			else
 			{
 				g_state = GS_START;
+
 				//answer = false;
 				displayTime = 300;
 				resultTime = 120;
@@ -128,7 +129,13 @@ void GameManager::Draw()
 
 
 	DrawFormatString(50, 500, 0xffffff, "g_state = %d", g_state, true, false);
-	DrawFormatString(50, 550, 0xffffff, "問%d", q_num, true, false);
+	DrawFormatString(50, 550, 0xffffff, "問%d", quiz.ID, true, false);
 	DrawFormatString(50, 600, 0xffffff, "正解発表まで%d", resultTime, true, false);
-	DrawFormatString(50, 650, 0xffffff, "次の問題まで%d", displayTime, true, false);	
+	DrawFormatString(50, 650, 0xffffff, "次の問題まで%d", displayTime, true, false);
+}
+
+void GameManager::SetPlayerAnswer(int _answer)
+{
+	answerNo = _answer;
+	g_state = GS_ANSWER;
 }
--- project/Source/GameManager.h
+++ project/Source/GameManager.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "../Library/GameObject.h"
+#include "QuizDefine.h"
 
 enum GAME_STATE
 {
@@ -9,14 +10,8 @@ enum GAME_STATE
 	GS_SWITCHING,
 };
 
-enum QUESTION_NUMBER
-{
-	Q_NULL,
-	Q_1,
-	Q_2,
-	Q_3
-};
 
+class QuizManager;
 class Question;
 class GameManager : public GameObject
 {
@@ -25,8 +20,10 @@ public:
 	~GameManager();
 	void Update()override;
 	void Draw()override;
+	//void SetQuiz(QuizManager* quizManager) { m_quizManager = quizManager; }
+	void SetPlayerAnswer(int _answer);
 
-
+	Quiz::Data GetQuiz() { return quiz; }
 private:
 	int answerNo;//プレイヤーの出した答え
 	int correctAnswerNo;//問題の答え
@@ -34,17 +31,17 @@ private:
 	int quest_num;//問題番号
 	int pushed;
 
-	static const int ANSWER_NUMBER_1 = 3;//問題の答えの番号(仮)
-	static const int ANSWER_NUMBER_2 = 1;//問題の答えの番号(仮)
-	static const int ANSWER_NUMBER_3 = 2;//問題の答えの番号(仮)
+	static const int ANSWER_NUMBER_1 = 2;//問題の答えの番号(仮)
+	static const int ANSWER_NUMBER_2 = 0;//問題の答えの番号(仮)
+	static const int ANSWER_NUMBER_3 = 1;//問題の答えの番号(仮)
 
 
 	GAME_STATE g_state;
 	Question* question;
-	QUESTION_NUMBER q_num;
+
 	int displayTime = 0;
 	int resultTime = 0;
 
-
-
+	QuizManager* m_quizManager;   // ★ クイズ管理
+	Quiz::Data quiz;
 };
